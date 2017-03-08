e45a7a976033d6d6c93d154821f13f78da0e2699
George Galt <george@galtfamily.net>
2017-03-08 17:59:09 -0500

    more server work

--- serverconnection.cpp
+++ serverconnection.cpp
@@ -4,3 +4,22 @@ ServerConnection::ServerConnection(QObject *parent) :
     QTcpSocket(parent)
 {
 }
+
+void ServerConnection::sendThermoData(thermostatData &m_thermostatData)
+{
+    m_thermostatData.setIndoorTemp(currentIndoorTemp);
+    m_thermostatData.setIndoorHumidity(currentIndoorHumidity);
+    m_thermostatData.setThermostatRange(currentTempRange);
+    m_thermostatData.setOutdoorTemp(m_weather->currentWeather().temperature());
+    m_thermostatData.setTimestamp(QDateTime::currentDateTime());
+
+    qint64 bytesToBeWritten = 0;
+    bytesToBeWritten = m_thermostatData.dataPayloadSize();
+
+    out << m_thermostatData;
+}
+
+void ServerConnection::Init()
+{
+
+}
--- serverconnection.h
+++ serverconnection.h
@@ -3,6 +3,9 @@
 
 #include <QObject>
 #include <QTcpSocket>
+#include <QMutex>
+#include <QMutexLocker>
+
 #include "thermostateventmodel.h"
 
 class ServerConnection : public QTcpSocket
@@ -10,13 +13,16 @@ class ServerConnection : public QTcpSocket
     Q_OBJECT
 public:
     explicit ServerConnection(QObject *parent = Q_NULLPTR);
+    void sendThermoData(thermostatData &m_thermostatData);
 
 signals:
     void updateThermostatEvents(void);
 
 public slots:
+    void Init(void);
 
 private:
+    QMutex mutex;
 };
 
 #endif // SERVERCONNECTION_H
